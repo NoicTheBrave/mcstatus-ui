54c9e7765d15ba8f86bb8f0f60ccc76737c58bb5
NoicTheBrave <nicpizza2000@gmail.com>
2024-04-25 17:19:22 -0500

    Not fully intigrated yet, but I BELIEVE these are the things I need to get it to work correctly now with the server - damn it, this is NOT working on school work but man, I am having fun - ChatGPT makes this SO much faster, too bad I am learning less even doing this in the process tho :/
    But atleast I am having some fun and this will serve as something practical in the future - I hope LOL

--- UI-Testing/testUI_Threadded_v4.py
+++ UI-Testing/testUI_Threadded_v4.py
@@ -53,6 +53,41 @@ def show_popup(button_index):
             time_text.config(state='disabled') #Disables user's ability to edit text field (Good)
             time.sleep(1) #Time delay for the counter
             seconds += 1
+            
+def getIPInfo(): 
+    server = JavaServer.lookup(test)# "tu-ece.playit.gg"))
+
+    '''
+        'status' is supported by all Minecraft servers that are version 1.7 or higher.
+        Don't expect the player list to always be complete, because many servers run
+        plugins that hide this information or limit the number of players returned or even
+        alter this list to contain fake players for purposes of having a custom message here.
+    '''
+    status = server.status()
+
+    print(f"The server has the following number players online: {status.players.online}") #and replied in {status.latency} ms")
+
+    """
+        # 'ping' is supported by all Minecraft servers that are version 1.7 or higher.
+        # It is included in a 'status' call, but is also exposed separate if you do not require the additional info.
+        #latency = server.ping()
+        #print(f"The server replied in {latency} ms")
+
+        # 'query' has to be enabled in a server's server.properties file!
+        # It may give more information than a ping, such as a full player list or mod information.
+    """
+    if (status.players.online != 0):
+    
+
+    print("\nAttempting to pull active player names...")
+    try:
+        query = server.query()
+        print(f"The server has the following players online: {', '.join(query.players.names)}")
+    except:
+        print("ERR: Cannot get name of players. please enable 'quere' in server.properties")
+    else:
+    print("No Players Online. Quere Skipped!")
+
 
     # Start a thread to update the time display
     threading.Thread(target=update_time, daemon=True).start()
