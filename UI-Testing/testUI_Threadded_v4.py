28d6632e43158f3abdf5f8df9f9c0489001f1b9c
NoicTheBrave <nicpizza2000@gmail.com>
2024-04-25 17:52:39 -0500

    Removed function that I was no longer using as refference :)

--- UI-Testing/testUI_Threadded_v4.py
+++ UI-Testing/testUI_Threadded_v4.py
@@ -98,42 +98,6 @@ def show_popup(button_index):
             #seconds += 1
     # Start a thread to update the time display
     threading.Thread(target=update_time, daemon=True).start()
-            
-def getIPInfo(): 
-    server = JavaServer.lookup(test)# "tu-ece.playit.gg"))
-
-    '''
-        'status' is supported by all Minecraft servers that are version 1.7 or higher.
-        Don't expect the player list to always be complete, because many servers run
-        plugins that hide this information or limit the number of players returned or even
-        alter this list to contain fake players for purposes of having a custom message here.
-    '''
-    status = server.status()
-
-    print(f"The server has the following number players online: {status.players.online}") #and replied in {status.latency} ms")
-
-    """
-        # 'ping' is supported by all Minecraft servers that are version 1.7 or higher.
-        # It is included in a 'status' call, but is also exposed separate if you do not require the additional info.
-        #latency = server.ping()
-        #print(f"The server replied in {latency} ms")
-
-        # 'query' has to be enabled in a server's server.properties file!
-        # It may give more information than a ping, such as a full player list or mod information.
-    """
-    if (status.players.online != 0):
-        print("\nAttempting to pull active player names...")
-        try:
-            query = server.query()
-            print(f"The server has the following players online: {', '.join(query.players.names)}")
-        except:
-            print("ERR: Cannot get name of players. please enable 'quere' in server.properties")
-    else:
-        print("No Players Online. Quere Skipped!")
-
-
-
-
 
 def close_program():
     root.destroy()
