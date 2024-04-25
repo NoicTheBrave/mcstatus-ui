2f4ac0fa548609376a99b0de32f43893e6ada2dd
NoicTheBrave <nicpizza2000@gmail.com>
2024-04-25 16:26:13 -0500

    If text file has an 'empty final row' (aka, a blank row, but the row above it has text in it), then all is normal, proceed, BUT! IF!!! IF IF IF the final row does not have this occuer, then this is an edge case that I just took care of - where I need to add a newline before the pgm slaps the IP in there, otherwise, the bottom IP address and the one you are trying to add would be merged - one after the other, cause pgm sees final row w/ NO text in it as the row not existing, but instead as the row before it w/ text with simply the new-line character @  the end of it (computers are goofy)

--- UI-Testing/testUI_Threadded_v3.py
+++ UI-Testing/testUI_Threadded_v3.py
@@ -42,7 +42,19 @@ def add_server_ip():
     new_ip = simpledialog.askstring("Add Server IP", "Enter the new IP address:")
     if new_ip is not None:
         try:
-            with open("iplist.txt", "a") as file:
+            with open("iplist.txt", "r+") as file:
+                lines = file.readlines()
+                
+                
+                print("LINES: " + str(len(lines)) + "\n")
+                print("FinalLineLen: " + str(len(lines[-1])) + "\n")
+                #print("FinalLine - Final Char: "+ str(int(lines[-1][-1])) + "\n")
+                print("-----\n") #Consider this the end of this debug chunk
+                
+                
+                #if lines[-1].strip():  # Check if the final row has text
+                if lines[-1][-1] != '\n':# Check if the "final row" has a newline @ the end of it (aka, blank final row)
+                    file.write("\n")  # Add a new blank row if not empty
                 file.write(new_ip + "\n")
         except Exception as e:
             messagebox.showerror("Error", f"Failed to add IP: {e}")
@@ -66,3 +78,4 @@ root.withdraw()  # Hide the root window
 main()
 
 root.mainloop()
+
