5d4519de09f00bb732926a799b2172d87b6246b6
NoicTheBrave <nicpizza2000@gmail.com>
2024-05-02 02:24:23 -0500

    Updated the demo pgm w/ some notes about storing someone's on/offline status... idk man, alot of things to consider - but since querying isnt erven ON for the UMS, IDC to look TOO much further into it for the time being since I cannot even test the bloody feature!

--- testPlayerNameStorage.py
+++ testPlayerNameStorage.py
@@ -22,6 +22,9 @@ def create_folders_and_files(name, content):
         file.write("\n" + str(content))
 
 # Example usage
-name = "bobby"
-content = "bobby Sucks"
+name = "bobby" #This would be the name of the player I can get from querying the server, IF they were online, I suppose (probably just run a FOR loop for those online for querying, and just update their status' accordingly)
+content = "bobby Sucks" #this would be any additional data I would wanna include about the player - such as their ONLINE status - we can imply they are OFFLINE if there is nothing written about them in the text file (no need to increase system storage to tell me someone is offline, I only really care whey they are ONLINE, UNLESS! I just REALLY wanna know for sure if they were offline... and I WAS infact pinging the server... then IG i would wanna know.... (frick) - maybe make that ASWELL a togglable option...?)
 create_folders_and_files(name,content)
+
+#NOTE: 
+# If I DO plan to toggle on/off storing if a player is OFFLINE, then I will need to PROBABLY make that it's own thread dedicated to that... and MAYBE even have it select players to record offliner status for - for example, i might only wanna know when Z is on hypixel, and NOT ALL 40k players, so I could just pick him... alternatively, if I DID, I need a select ALL button as well (I am not about to click 40k buttons just for that data...)
