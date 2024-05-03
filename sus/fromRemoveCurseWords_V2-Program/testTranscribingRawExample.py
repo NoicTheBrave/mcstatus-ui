33b69f4438d699d8b22e3dc8bc9cadb3f7a745cf
NoicTheBrave <nicpizza2000@gmail.com>
2024-05-02 23:36:07 -0500

    Off track again - but happy... REGUARDLESS - trying to get raw example of this thing working for hecks and giggles ig... I really should stop and get aback towork

---
+++ sus/fromRemoveCurseWords_V2-Program/testTranscribingRawExample.py
@@ -0,0 +1,3 @@
+from TranscriptV3 import *
+
+audioname = "microphone-results.wav" # might not like it, if it is NOT mono-channel. if thats the case, an error will occur, and audio will need to be fed thru mono-channel converter pgm used for removing curse words as well.... ug
