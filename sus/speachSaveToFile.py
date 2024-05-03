eab6d096002325207d46081949b9d4dd7a465a02
NoicTheBrave <nicpizza2000@gmail.com>
2024-05-02 22:39:40 -0500

    The way its made now, it will FULLY replace the .wav file, and ONLY now make .wav files from my audio, cause as far as I am concerned, its all i need atm (idk what the other formats are for, let along why they matter and what not :P )

--- sus/speachSaveToFile.py
+++ sus/speachSaveToFile.py
@@ -12,18 +12,18 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
-# write audio to a RAW file
+""" # write audio to a RAW file
 with open("microphone-results.raw", "wb") as f:
     f.write(audio.get_raw_data())
-
+ """
 # write audio to a WAV file
 with open("microphone-results.wav", "wb") as f:
     f.write(audio.get_wav_data())
 
-# write audio to an AIFF file
+""" # write audio to an AIFF file
 with open("microphone-results.aiff", "wb") as f:
     f.write(audio.get_aiff_data())
 
 # write audio to a FLAC file
 with open("microphone-results.flac", "wb") as f:
-    f.write(audio.get_flac_data())
+    f.write(audio.get_flac_data()) """
